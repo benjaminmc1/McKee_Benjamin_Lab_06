645d0e1adbc48a2313b1ac650ce932ebd4e9f8fa
Benjamin <bmckee566@gmail.com>
2023-08-07 22:21:29 -0400

    CtoFConverter finish

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -2,6 +2,23 @@ import java.util.Scanner;
 
 public class CtoFConverter {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double celsius = 0;
+        double fahrenheit = 0;
         
+        System.out.println("Enter the temperature in celsius: ");
+
+        do {
+            celsius = in.nextDouble();
+            if(!in.hasNextDouble()) {
+                System.out.println("Invalid entry. Try again.");
+            }
+        }while(!in.hasNextDouble());
+
+        fahrenheit = (celsius * (9 / 5)) + 32;
+        System.out.println("The temperature in fahrenheit is " + fahrenheit + " degrees.");
+
+        in.close();
     }
 }
