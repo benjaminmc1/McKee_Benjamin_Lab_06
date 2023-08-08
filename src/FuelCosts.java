923ece419380905c3cc2242e2357255c659cdb0d
Benjamin <bmckee566@gmail.com>
2023-08-07 22:22:26 -0400

    FuelCosts init

---
+++ src/FuelCosts.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FuelCosts {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        
+
+        in.close();
+    }
+}
